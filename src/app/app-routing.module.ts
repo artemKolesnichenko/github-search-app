604bbd02cd80092de4fb6ee0ff8dfd9e24e4e6af
Artem Kolesnichenko <kolesnichenko.2001@gmail.com>
2022-06-23 19:16:14 +0300

    navbar was finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlocksComponent } from './components/blocks/blocks.component';
+import { DetailsComponent } from './components/details/details.component';
+import { TableComponent } from './components/table/table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'blocks', component: BlocksComponent},
+  {path: 'table', component: TableComponent},
+  {path: 'blocks/:id', component: DetailsComponent},
+  {path: 'table/:id', component: DetailsComponent},
+  {path: '**', redirectTo: 'blocks', pathMatch: 'full'},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlocksComponent } from './components/blocks/blocks.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TableComponent } from './components/table/table.component';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlocksComponent,
+    NavbarComponent,
+    TableComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
